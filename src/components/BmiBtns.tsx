55771da4ffb8b839e25177805835fde3063f5420
EzanAsif <ezanasif1972@gmail.com>
2021-04-28 19:54:19 +0500

    Final Commit

--- src/components/BmiBtns.tsx
+++ src/components/BmiBtns.tsx
@@ -2,24 +2,28 @@ import React from "react";
 import { IonRow, IonCol, IonIcon, IonButton } from "@ionic/react";
 import { calculatorOutline, refreshOutline } from "ionicons/icons";
 
-const BmiBtns: React.FC<{ onCalc: () => void;  onReset: () => void }> = (
+const BmiBtns: React.FC<{ onCalc: () => void; onReset: () => void }> = (
   props
 ) => {
   return (
-    <IonRow>
-      <IonCol className="ion-text-left">
-        <IonButton onClick={props.onCalc}>
-          <IonIcon icon={calculatorOutline} slot="start" />
-          Calculate
-        </IonButton>
-      </IonCol>
-      <IonCol className="ion-text-right">
-        <IonButton onClick={props.onReset}>
-          <IonIcon icon={refreshOutline} slot="start" />
-          Reset
-        </IonButton>
-      </IonCol>
-    </IonRow>
+    <>
+      <IonRow className="padding">
+        <IonCol className="ion-text-center">
+          <IonButton onClick={props.onCalc}>
+            <IonIcon icon={calculatorOutline} slot="start" />
+            Calculate
+          </IonButton>
+        </IonCol>
+      </IonRow>
+      <IonRow className="padding">
+        <IonCol className="ion-text-center">
+          <IonButton fill = "outline" onClick={props.onReset}>
+            <IonIcon icon={refreshOutline} slot="start" />
+            Reset
+          </IonButton>
+        </IonCol>
+      </IonRow>
+    </>
   );
 };
 
